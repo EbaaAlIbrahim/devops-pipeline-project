418918404a8f5f0372a1364e90c17518aedd46c8
Ebaa AlIbrahim <ebaaibrahime@gmail.com>
2026-05-27 18:02:06 +0300

    feat: setup app and initial CI pipeline

---
+++ test.js
@@ -0,0 +1,12 @@
+// A simple test script
+console.log("Running automated unit tests...");
+
+const appStatus = "healthy";
+
+if (appStatus === "healthy") {
+    console.log("SUCCESS: All tests passed!");
+    process.exit(0); // Exit code 0 means "Success" to the pipeline
+} else {
+    console.log("FAILURE: System is unhealthy!");
+    process.exit(1); // Any exit code above 0 tells the pipeline to stop because of an error
+}
